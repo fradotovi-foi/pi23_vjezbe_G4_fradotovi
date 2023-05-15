a9f2869f60a4963ebfa4cf04a8e17458453230f1
Filip Radotović <fradotovi@student.foi.hr>
2023-05-15 17:15:53 +0200

    Implementacija klase Evaluation

--- Repositories/EvaluationRepository.cs
+++ Repositories/EvaluationRepository.cs
@@ -11,40 +11,40 @@ namespace Evaluation_Manager.Repositories
 {
     public class EvaluationRepository
     {
-        //public static Evaluation GetEvaluation(Student student, Activity activity)
-        //{
-        //    Evaluation evaluation = null;
-        //    string sql = $"SELECT * FROM Evaluations WHERE IdStudents = {student.Id} AND IdActivities = {activity.Id}";
-        //    DB.OpenConnection();
-        //    var reader = DB.GetDataReader(sql);
-        //    if (reader.HasRows)
-        //    {
-        //        reader.Read();
-        //        evaluation = CreateObject(reader);
-        //        reader.Close();
-        //    }
-        //    DB.CloseConnection();
-        //    return evaluation;
-        //}
-        //public static List<Evaluation> GetEvaluations(Student student)
-        //{
-        //    List<Evaluation> evaluations = new List<Evaluation>();
-        //    string sql = $"SELECT * FROM Evaluations WHERE IdStudents = {student.Id}";
-        //    DB.OpenConnection();
-        //    var reader = DB.GetDataReader(sql);
-        //    while (reader.Read())
-        //    {
-        //        Evaluation evaluation = CreateObject(reader);
-        //        evaluations.Add(evaluation);
-        //    }
-        //    reader.Close();
-        //    DB.CloseConnection();
-        //    return evaluations;
-        //}
-        /*private static Evaluation CreateObject(SqlDataReader reader)
+        public static Evaluation GetEvaluation(Student student, Activity activity)
+        {
+            Evaluation evaluation = null;
+            string sql = $"SELECT * FROM Evaluations WHERE IdStudents = {student.Id} AND IdActivities = {activity.Id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                evaluation = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return evaluation;
+        }
+        public static List<Evaluation> GetEvaluations(Student student)
+        {
+            List<Evaluation> evaluations = new List<Evaluation>();
+            string sql = $"SELECT * FROM Evaluations WHERE IdStudents = {student.Id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Evaluation evaluation = CreateObject(reader);
+                evaluations.Add(evaluation);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return evaluations;
+        }
+        private static Evaluation CreateObject(SqlDataReader reader)
         {
             int idActivities = int.Parse(reader["IdActivities"].ToString());
-            var activity = ActivityRepository.GetActivity(idActivities);
+            var activity = ActivitiesRepository.GetActivity(idActivities);
 
             int idStudents = int.Parse(reader["IdStudents"].ToString());
             var student = StudentRepository.GetStudent(idStudents);
@@ -64,6 +64,6 @@ namespace Evaluation_Manager.Repositories
                 Points = points
             };
             return evaluation;
-        }*/
+        }
     }
 }
