2d567b63e865d2bf60dc713b58239800f6983d77
Filip Radotović <fradotovi@student.foi.hr>
2023-05-15 17:24:42 +0200

    Forma za evaluacije povezana s repozitorijom evaluacije

--- frmEvaluateStudent.cs
+++ frmEvaluateStudent.cs
@@ -42,6 +42,26 @@ namespace Evaluation_Manager
            currentActivity.MaxPoints;
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+
+            var evaluation = EvaluationRepository.GetEvaluation(Evaluatingstudent, currentActivity);
+
+            if(evaluation != null)
+            {
+                txtTeacher.Text = evaluation.Evaluator.ToString();
+
+                txtEvaluationDate.Text = evaluation.EvaluationDate.ToString();
+
+                numPoints.Value = evaluation.Points;
+            }
+            else
+            {
+                txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "";
+                numPoints.Value = 0;
+            }
+
+
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
