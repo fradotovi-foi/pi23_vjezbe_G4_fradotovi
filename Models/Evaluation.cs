f2e603c014ba2ee3fafdb79bd57053c5477a9cab
Filip Radotović <fradotovi@student.foi.hr>
2023-05-15 17:07:40 +0200

    Dodana klasa Evaluation

---
+++ Models/Evaluation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models
+{
+    public class Evaluation
+    {
+        public Activity Activity { get; set; }
+
+        public Student Student { get; set; }
+
+        public Teacher Evaluator { get; set; }
+        public DateTime EvaluationDate { get; set; }
+
+        public int Points { get; set; }
+
+
+        public bool IsSufficientForGrade(int points)
+        {
+            return Points >= Activity.MinPointsForGrade;
+        }
+
+        public bool IsSufficientForSignature(int points)
+        {
+            return Points >= Activity.MinPointsForSignature;
+        }
+
+
+    }
+}
